7234b3dd82d20f924140d126cd92b9c60259c865
Your Name <k.bristi057@gmail.com>
2025-08-06 20:29:36 +0600

    added errorpage

--- src/component/ErrorPage/ErrorPage.jsx
+++ src/component/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+import animation from "../../assets/404 error page with cat.json";
+import Lottie from "lottie-react";
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="mt-34 flex flex-col items-center">
+      <h2 className="text-3xl text-center text-orange-700 font-bold">
+        Error Page
+      </h2>
+      <div className="w-[30%] mx-auto mt-6">
+        <Lottie animationData={animation} loop={true} />
+      </div>
+      <Link to="/" className="text-sm text-gray-600 mt-7">
+        <span className="text-center"> Go Back</span>
+      </Link>
+    </div>
+  );
 }
